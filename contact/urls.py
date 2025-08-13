c22627ef635450dae1ac521748d8dbc9248bafe6
Brunno Bronner <brunner.bronner@gmail.com>
2025-08-13 17:39:26 -0300

    mexendo em urls e em models

--- contact/urls.py
+++ contact/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path
+from django.conf.urls.static import static
 from contact import views
-
+from django.conf import settings
 app_name = 'contact'
 
 urlpatterns = [
     path('',views.index, name = 'index')
 ]
+
+urlpatterns += static(settings.MEDIA_URL, document_root= settings.MEDIA_ROOT)
